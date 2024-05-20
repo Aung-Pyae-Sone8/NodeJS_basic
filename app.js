e3170a9154c09c3fe688d895684d3f9cde42cf82
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-05-20 12:15:15 +0630

    custom and third-party middleware

--- app.js
+++ app.js
@@ -1,54 +1,60 @@
 const express = require('express');
+let morgan = require('morgan');
 
 const app = express();
 
 app.set('views', './views');
 app.set('view engine', 'ejs')
 
-app.use((req,res,next) => {
-    console.log('first middleware is running');
-    next();
-})
+// custom package 
+// let logger = (env) => {
+//     return (req, res, next) => {
+//         if(env === 'dev') {
+//             console.log(`${req.method} ${req.originalUrl} --`)
+//         }
+//         next();
+//     }
+// };
 
-app.use((req,res,next) => {
-    console.log('second middleware is runnging');
-    next();
-})
+// app.use(logger())
 
-app.get('/',(req,res) => {
+// morgan package 
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
 
     let blogs = [
-        {title : 'Blog title 1', intro : 'this is blog intro 1' },
-        {title : 'Blog title 2', intro : 'this is blog intro 2' },
-        {title : 'Blog title 3', intro : 'this is blog intro 3' },
+        { title: 'Blog title 1', intro: 'this is blog intro 1' },
+        { title: 'Blog title 2', intro: 'this is blog intro 2' },
+        { title: 'Blog title 3', intro: 'this is blog intro 3' },
     ]
 
-    res.render('home',{
+    res.render('home', {
         // blogs : blogs 
         blogs,
-        title : "Home",
+        title: "Home",
     })
 })
 
-app.get('/about',(req,res) => {
-    res.render('about',{
-        title : "About"
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About"
     })
 })
 
-app.get('/contact',(req,res) => {
+app.get('/contact', (req, res) => {
     res.render('contact', {
-        title : "Contact"
+        title: "Contact"
     })
 })
 
-app.use((req,res) => {
+app.use((req, res) => {
     res.status(404).render('404', {
-        title : "404 Not Found"
+        title: "404 Not Found"
     })
 })
 
 
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log('app is running on port 3000')
 })
